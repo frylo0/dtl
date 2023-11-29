22fcf30be0e565ac3179c4573f41bfc81202fa32
fritylo <to.fritylo@gmail.com>
2023-11-29 17:23:36 +0300

    feat: added ren command

---
+++ commands/ren.mjs
@@ -0,0 +1,25 @@
+import process from 'process';
+import path from 'path';
+import fs from 'fs';
+
+import { logInfo, genWarn, logOk } from "../utils/logger.mjs";
+import { ask } from '../utils/prompt.mjs';
+import templateApi from '../utils/template-api.mjs';
+
+export default async function (argv, config) {
+    const template = templateApi(config);
+
+    if (argv.files.length === 0) {
+        argv.files.push(argv.name);
+    }
+
+    const name = argv.name;
+    const newName = argv.newName;
+    const files = argv.files;
+
+    template.rename(name, newName, files);
+    
+    logOk(`\nSuccessfully!\n`);
+
+    process.exit();
+};
